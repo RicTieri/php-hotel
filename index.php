3a7deb2be54fd528ffad2969c239228ad3fd8454
RicTieri <tieri.riccardo@gmail.com>
2024-01-15 17:22:30 +0100

    added bootstrap table

--- index.php
+++ index.php
@@ -44,6 +44,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>php hotel</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
   <ul>
@@ -53,5 +54,31 @@
       </li>
     <?php } ?>
   </ul>
+
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Parking</th>
+      <th scope="col">Vote</th>
+      <th scope="col">Distance to center</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($hotels as $hotel) { ?>
+    <tr>
+      <th scope="row">1</th>
+      <td> <?php echo $hotel['name'] ?></td>
+      <td> <?php echo $hotel['description'] ?></td>
+      <td> <?php if( $hotel['parking']) { echo 'avaible';} else{ echo 'not avaible';}?></td>
+      <td> <?php echo $hotel['vote'] ?></td>
+      <td> <?php echo $hotel['distance_to_center'] ?></td>
+    </tr>
+    <?php } ?>
+
+  </tbody>
+</table>
 </body>
 </html>
