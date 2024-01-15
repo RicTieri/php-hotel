dd4ad0a8bc55b0e63f7f5f91623f8e74c7c13ce8
RicTieri <tieri.riccardo@gmail.com>
2024-01-15 17:34:19 +0100

    added base filter

--- index.php
+++ index.php
@@ -40,45 +40,60 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>php hotel</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+
 <body>
   <ul>
     <?php foreach ($hotels as $hotel) { ?>
-      <li>
-        <?php echo $hotel['name'] . ' ' . $hotel['description'] . ' ' . $hotel['parking'] . ' ' . $hotel['vote'] . ' ' .  $hotel['distance_to_center']?>
-      </li>
+    <li>
+      <?php echo $hotel['name'] . ' ' . $hotel['description'] . ' ' . $hotel['parking'] . ' ' . $hotel['vote'] . ' ' .  $hotel['distance_to_center']?>
+    </li>
     <?php } ?>
   </ul>
 
-  <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Name</th>
-      <th scope="col">Description</th>
-      <th scope="col">Parking</th>
-      <th scope="col">Vote</th>
-      <th scope="col">Distance to center</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($hotels as $hotel) { ?>
-    <tr>
-      <th scope="row">1</th>
-      <td> <?php echo $hotel['name'] ?></td>
-      <td> <?php echo $hotel['description'] ?></td>
-      <td> <?php if( $hotel['parking']) { echo 'avaible';} else{ echo 'not avaible';}?></td>
-      <td> <?php echo $hotel['vote'] ?></td>
-      <td> <?php echo $hotel['distance_to_center'] ?></td>
-    </tr>
-    <?php } ?>
+  <form action="./index.php" method="GET">
+    <select name="is_park" id="is_park">
+      <option value="all" selected>all</option>
+      <option value="0">avaible</option>
+      <option value="1">not avaible</option>
+    </select>
+    <button type="submit">Filter</button>
+  </form>
 
-  </tbody>
-</table>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Description</th>
+        <th scope="col">Parking</th>
+        <th scope="col">Vote</th>
+        <th scope="col">Distance to center</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php if($_GET['is_park'] == 'all') { ?>
+      <?php foreach ($hotels as $hotel) { ?>
+      <tr>
+        <th scope="row">1</th>
+        <td> <?php echo $hotel['name'] ?></td>
+        <td> <?php echo $hotel['description'] ?></td>
+        <td> <?php if( $hotel['parking']) { echo 'avaible';} else{ echo 'not avaible';}?></td>
+        <td> <?php echo $hotel['vote'] ?></td>
+        <td> <?php echo $hotel['distance_to_center'] ?></td>
+      </tr>
+      <?php } ?>
+      <?php } ?>
+
+    </tbody>
+  </table>
 </body>
+
 </html>
